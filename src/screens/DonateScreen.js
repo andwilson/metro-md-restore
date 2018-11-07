d557a998f79b07ae3ebe1f55fec745f67151d584
Andrew Wilson <5093707+andwilson@users.noreply.github.com>
2018-11-07 10:50:13 -0800

    Further styling of donations page

--- src/screens/DonateScreen.js
+++ src/screens/DonateScreen.js
@@ -33,98 +33,67 @@ const DonateScreen = () => (
       <Text>Call 301-926-4104 OR</Text>
       <Text>Email ReStore@HabitatMM.org</Text>
       <Header>What we accept</Header>
-      <Text>
-        <ItemTitle>Appliances All appliances accepted</ItemTitle>
-        <ItemDetail>
-          We are no longer able to accept dishwashers of any kind, or
-          refrigerators over 10 years old.
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Area Rugs</ItemTitle>
-        <ItemDetail>
-          Must be under 11 feet in length, new or excellent condition
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Cabinets</ItemTitle>
-        <ItemDetail>
-          Must be complete units, sTextucturally intact; must have doors,
-          shelving, display racks, etc.; single cabinets (pieces) will be taken
-          on a case by case basis
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Flooring material</ItemTitle>
-        <ItemDetail>
-          Vinyl no less than 25 sq. ft., new hardwood complete boxes, ceramic
-          tile no less than 25 square feet.&nbsp; We do not accept scraps of any
-          kind of flooring materials, exception being reclaimed hardwood
-          flooring, must be at least 90 square feet.
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>
-          Furniture Sofas, love seats, coffee tables, bedroom furniture, dining
-          room
-        </ItemTitle>
-        <ItemDetail>
-          Must be in re-salable condition (no rips, stains or tears.)
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Household Items</ItemTitle>
-        <ItemDetail>Must be clean and unbroken</ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Lawnmowers &amp; Lawn Equipment</ItemTitle>
-        <ItemDetail>Must be working with all parts</ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>
-          Lighting Kitchen/dining room chandeliers and sconces, compact
-          florescent bulbs, lamps (standing/table top)
-        </ItemTitle>
-        <ItemDetail>Must be working with all parts</ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Lumber</ItemTitle>
-        <ItemDetail>
-          Must be between 3 – 8 feet in length, we cannot accept lumber with
-          nails
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Mirrors</ItemTitle>
-        <ItemDetail>
-          Mirrors with frames only. We do not accept unframed mirrors or glass
-          due to safety risks
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Plumbing</ItemTitle>
-        <ItemDetail>
-          Must be complete, very clean, and in good condition; heavy duty faucet
-          fixtures, faucets, parts, pedestal sinks, toilets (1.6 gallon/low
-          flush only)
-        </ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Roofing</ItemTitle>
-        <ItemDetail>New only</ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Small ElecTextonics</ItemTitle>
-        <ItemDetail>Must be working with all parts</ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Tools &amp; Hardware</ItemTitle>
-        <ItemDetail>Must be working with all parts</ItemDetail>
-      </Text>
-      <Text>
-        <ItemTitle>Windows</ItemTitle>
-        <ItemDetail>We cannot accept sashes</ItemDetail>
-      </Text>
+      <ItemTitle>Appliances All appliances accepted</ItemTitle>
+      <ItemDetail>
+        We are no longer able to accept dishwashers of any kind, or
+        refrigerators over 10 years old.
+      </ItemDetail>
+      <ItemTitle>Area Rugs</ItemTitle>
+      <ItemDetail>
+        Must be under 11 feet in length, new or excellent condition
+      </ItemDetail>
+      <ItemTitle>Cabinets</ItemTitle>
+      <ItemDetail>
+        Must be complete units, sTextucturally intact; must have doors,
+        shelving, display racks, etc.; single cabinets (pieces) will be taken on
+        a case by case basis
+      </ItemDetail>
+      <ItemTitle>Flooring material</ItemTitle>
+      <ItemDetail>
+        Vinyl no less than 25 sq. ft., new hardwood complete boxes, ceramic tile
+        no less than 25 square feet.&nbsp; We do not accept scraps of any kind
+        of flooring materials, exception being reclaimed hardwood flooring, must
+        be at least 90 square feet.
+      </ItemDetail>
+      <ItemTitle>
+        Furniture Sofas, love seats, coffee tables, bedroom furniture, dining
+        room
+      </ItemTitle>
+      <ItemDetail>
+        Must be in re-salable condition (no rips, stains or tears.)
+      </ItemDetail>
+      <ItemTitle>Household Items</ItemTitle>
+      <ItemDetail>Must be clean and unbroken</ItemDetail>
+      <ItemTitle>Lawnmowers &amp; Lawn Equipment</ItemTitle>
+      <ItemDetail>Must be working with all parts</ItemDetail>
+      <ItemTitle>
+        Lighting Kitchen/dining room chandeliers and sconces, compact florescent
+        bulbs, lamps (standing/table top)
+      </ItemTitle>
+      <ItemDetail>Must be working with all parts</ItemDetail>
+      <ItemTitle>Lumber</ItemTitle>
+      <ItemDetail>
+        Must be between 3 – 8 feet in length, we cannot accept lumber with nails
+      </ItemDetail>
+      <ItemTitle>Mirrors</ItemTitle>
+      <ItemDetail>
+        Mirrors with frames only. We do not accept unframed mirrors or glass due
+        to safety risks
+      </ItemDetail>
+      <ItemTitle>Plumbing</ItemTitle>
+      <ItemDetail>
+        Must be complete, very clean, and in good condition; heavy duty faucet
+        fixtures, faucets, parts, pedestal sinks, toilets (1.6 gallon/low flush
+        only)
+      </ItemDetail>
+      <ItemTitle>Roofing</ItemTitle>
+      <ItemDetail>New only</ItemDetail>
+      <ItemTitle>Small ElecTextonics</ItemTitle>
+      <ItemDetail>Must be working with all parts</ItemDetail>
+      <ItemTitle>Tools &amp; Hardware</ItemTitle>
+      <ItemDetail>Must be working with all parts</ItemDetail>
+      <ItemTitle>Windows</ItemTitle>
+      <ItemDetail>We cannot accept sashes</ItemDetail>
       <Header>Other donations</Header>
       <Text>
         You can also help by making{" "}
@@ -180,6 +149,8 @@ const ItemTitle = styled.Text`
 
 const ItemDetail = styled.Text`
   color: ${props => props.theme.colors.dark};
+  margin-top: 2;
+  margin-bottom: 10;
 `;
 
 const OpenWebPage = styled.Text`
